47cd30b849c1374b4277273470fcacd54ef5188a
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-03-27 17:25:05 -0400

    Search State functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import Card from "./Components/Card/Card";
 import SearchBar from "./Components/SearchBar/SearchBar";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <div className="container">
       <div className="title">
         <h1>Github Showcase</h1>
       </div>
-      <SearchBar />
-      <Card />
+      <SearchBar search={search} setSearch={setSearch} />
+      {!search && <Card />}
     </div>
   );
 }
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { BiSearchAlt } from "react-icons/bi";
 
-const SearchBar = () => {
+const SearchBar = ({ search, setSearch }) => {
   return (
     <div className="searchbar">
       <form className="searchbar__form">
         <BiSearchAlt className="searchbar__icon" />
         <input
+          value={search}
           type="text"
           className="searchbar__input"
           placeholder="Search for a repo..."
+          onChange={(e) => setSearch(e.target.value)}
         />
         <button type="submit" className="searchbar__button">
           {" "}
