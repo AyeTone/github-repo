b5155f901448bd6978e535c3260c581e017f5e03
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-03-27 16:37:27 -0400

    Title Added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import Card from "./Components/Card/Card";
 
 function App() {
   return (
-    <div>
+    <>
+      <div className="title">
+        <h1>Github Showcase</h1>
+      </div>
       <Card />
-    </div>
+    </>
   );
 }
 
