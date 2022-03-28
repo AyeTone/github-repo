bd6d35b2c99bd8d4c4e95347994ef0596517a470
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-03-27 20:52:31 -0400

    Repolist moblie complete

--- src/Components/RepoList/RepoList.jsx
+++ src/Components/RepoList/RepoList.jsx
@@ -1,13 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const RepoList = ({ username }) => {
+  const [repos, setRepos] = useState([]);
   useEffect(() => {
     fetch(
       `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
-    );
+    )
+      .then((res) => res.json())
+      .then((data) => setRepos(data));
   }, []);
 
-  return <div>RepoList</div>;
+  return (
+    <div className="repolist">
+      <div className="repolist__container">
+        {repos.map((repo, id) => (
+          <li key={id}>
+            {" "}
+            <a href={repo.html_url} target="_blank">
+              {repo.name}
+            </a>
+          </li>
+        ))}
+      </div>
+      {/* <button onClick={(e) => console.log(repos)}> console</button> */}
+    </div>
+  );
 };
 
 export default RepoList;
